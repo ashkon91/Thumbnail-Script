3fa4544fbd5d85c388dc688d38b7056b79d34ce7
Ashkon Honardoost <ashkon91@gmail.com>
2016-09-15 05:46:56 -0700

    Newest Vewsion

--- thumbnailgenerator.jsx
+++ thumbnailgenerator.jsx
@@ -6,22 +6,25 @@ var charlay1 = doc.activeLayer;
 var charlay2 = doc.activeLayer;
 var group = layer.parent.layers;
 var count = 1;
-
+var name1;
+var name2;
 function savePNG(player1,player2,char1,char2){
 	var pngOptions = new PNGSaveOptions();
 	var path = File(count + ".png");
 	count++;
 	doc.saveAs(path, pngOptions, true, Extension.LOWERCASE);
-	alert(path);
+	name1="GAMERTAG #1";
+	name2 = "GAMERTAG #2";
 }
 
-function switchChar1(char1){
+function switchChar1(char1, color1){
 	//if(layer.kind != LayerKind.TEXT)layer.visible = false;
-	var layerSetRef = doc.layerSets.getByName("Characters_Left");
+	var charFolder = doc.layerSets.getByName("Characters_Left");
+	var layerSetRef = charFolder.layerSets.getByName(char1);	
 	group = layerSetRef.layers;
 	for(var i = 0; i < group.length; i++)
 	{
-		if(group[i].name == char1)
+		if(group[i].name == color1)
 		{
 			doc.activeLayer = group[i];
 			charlay1 = doc.activeLayer;
@@ -30,13 +33,14 @@ function switchChar1(char1){
 	}
 }
 
-function switchChar2(char2){
+function switchChar2(char2, color2){
 	//if(layer.kind != LayerKind.TEXT)layer.visible = false;
-	var layerSetRef = doc.layerSets.getByName("Characters_Right");
+	var charFolder = doc.layerSets.getByName("Characters_Right");
+	var layerSetRef = charFolder.layerSets.getByName(char2);
 	group = layerSetRef.layers;
 	for(var i = 0; i < group.length; i++)
 	{
-		if(group[i].name == char2)
+		if(group[i].name == color2)
 		{
 			doc.activeLayer = group[i];
 			charlay2 = doc.activeLayer;
@@ -50,6 +54,10 @@ function changePlayerName(layerName, newText){
 	var text = layerSetRef.layers.getByName(layerName);
 	if(text.kind == LayerKind.TEXT) text.textItem.contents = newText;
 	text.visible = true;
+	if(layerName == "GAMERTAG #1") 
+		name1 = text;
+	else 
+		name2 = text;
 }
 
 //CSV 
@@ -66,16 +74,18 @@ var lineData = csvString[s].split(",");
 //Process each line of data.
 var player1 = lineData[0];
 var char1 = lineData[1];
-var player2 = lineData[2];
-var char2 = lineData[3];
+var color1 = lineData[2];
+var player2 = lineData[3];
+var char2 = lineData[4];
+var color2 = lineData[5];
 
 //Switch Characters
-switchChar1(char1);
-switchChar2(char2);
+switchChar1(char1,color1);
+switchChar2(char2,color2);
 
 //Change player names
-changePlayerName("name1",player1);
-changePlayerName("name2",player2);
+changePlayerName("GAMERTAG #1",player1);
+changePlayerName("GAMERTAG #2",player2);
 
 //Save photo
 savePNG(player1,player2,char1,char2);
